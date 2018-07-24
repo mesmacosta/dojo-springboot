a077877e184f7ac10780cee159dfc8c53af75f09
mycelo <mycelo@gmail.com>
2018-07-24 17:44:33 -0300

    config estrutura

--- playlist-app/src/main/java/com/dojo/springboot/playlists/ApplicationPlaylist.java
+++ playlist-app/src/main/java/com/dojo/springboot/playlists/ApplicationPlaylist.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApplicationMusic {
+public class ApplicationPlaylist {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApplicationMusic.class, args);
+		SpringApplication.run(ApplicationPlaylist.class, args);
 		
 	}
 
