48e0db01a81e2537913a9c089c0881585873a835
Marcelo Miranda <mmiranda@ciandt.com>
2018-08-09 11:18:38 -0300

    implementação netflix oss

--- music-app/src/main/java/com/dojo/springboot/musicas/ApplicationMusic.java
+++ music-app/src/main/java/com/dojo/springboot/musicas/ApplicationMusic.java
@@ -3,11 +3,13 @@ package com.dojo.springboot.musicas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 
 @SpringBootApplication
 @EnableCaching
+@EnableDiscoveryClient
 public class ApplicationMusic {
 
 	public static void main(String[] args) {
---
+++ springboot-eureka-server/src/main/java/br/com/fsandes/eurekaserver/ApplicationEurekaServer.java
@@ -0,0 +1,17 @@
+package br.com.fsandes.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ApplicationEurekaServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationEurekaServer.class, args);
+
+	}
+
+}
